b793fb100d4a1b236a8b37311831edf79be748bb
B-Evans99 <4bronwenevans@gmail.com>
2020-02-29 14:15:31 -0800

    feat(App.js, Clock.js, SettingController.js, Timer.js): add placeholder components for the main elements of the design

---
+++ src/components/Clock.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Clock() {
+  return <h1>clock</h1>;
+}
---
+++ src/components/SettingController.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function SettingController() {
+  return <h1>Settings</h1>;
+}
