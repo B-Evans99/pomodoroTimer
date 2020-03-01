bb2431b05f0510759eb9a6f89a1ad7c4bab13f49
B-Evans99 <4bronwenevans@gmail.com>
2020-03-01 03:16:36 -0800

    feat(SettingController.js): organize setting inputs into a basic structure

--- src/components/SettingController.js
+++ src/components/SettingController.js
@@ -23,56 +23,69 @@ export default function SettingController({
 }) {
   return (
     <div className="sessionController">
-      <FontAwesomeIcon
-        icon={faPlay}
-        size="lg"
-        onClick={start}
-      ></FontAwesomeIcon>
-      <FontAwesomeIcon
-        icon={faPause}
-        size="lg"
-        onClick={pauseTimer}
-      ></FontAwesomeIcon>
-      <FontAwesomeIcon
-        icon={faBackward}
-        size="lg"
-        onClick={() => {
-          setTime(500);
-        }}
-      ></FontAwesomeIcon>
-      <FontAwesomeIcon
-        icon={show ? faEye : faEyeSlash}
-        size="lg"
-        onClick={() => {
-          setShow(show => {
-            return !show;
-          });
-        }}
-      ></FontAwesomeIcon>
-      <input
-        type="text"
-        value={workTime}
-        onChange={e => {
-          let newVal = parseInt(e.target.value);
-          setWorkTime(newVal);
-        }}
-      ></input>
-      <input
-        type="text"
-        value={restTime}
-        onChange={e => {
-          let newVal = parseInt(e.target.value);
-          setRestTime(newVal);
-        }}
-      ></input>
-      <input
-        type="text"
-        value={longRestTime}
-        onChange={e => {
-          let newVal = parseInt(e.target.value);
-          setLongRestTime(newVal);
-        }}
-      ></input>
+      <div className="iconBar">
+        <FontAwesomeIcon
+          icon={faPlay}
+          size="lg"
+          onClick={start}
+        ></FontAwesomeIcon>
+        <FontAwesomeIcon
+          icon={faPause}
+          size="lg"
+          onClick={pauseTimer}
+        ></FontAwesomeIcon>
+        <FontAwesomeIcon
+          icon={faBackward}
+          size="lg"
+          onClick={() => {
+            setTime(500);
+          }}
+        ></FontAwesomeIcon>
+        <FontAwesomeIcon
+          icon={show ? faEye : faEyeSlash}
+          size="lg"
+          onClick={() => {
+            setShow(show => {
+              return !show;
+            });
+          }}
+        ></FontAwesomeIcon>
+      </div>
+      <div className="sessionControlBox">
+        <div className="sessionBox">
+          <h3>Work Session</h3>
+          <input
+            type="text"
+            value={workTime}
+            onChange={e => {
+              let newVal = parseInt(e.target.value);
+              setWorkTime(newVal);
+            }}
+          ></input>
+        </div>
+        <div className="sessionBox">
+          <h3>Rest Session</h3>
+          <input
+            type="text"
+            value={restTime}
+            onChange={e => {
+              let newVal = parseInt(e.target.value);
+              setRestTime(newVal);
+            }}
+          ></input>
+        </div>
+        <div className="sessionBox">
+          <h3>Long Rest Session</h3>
+          <input
+            type="text"
+            value={longRestTime}
+            onChange={e => {
+              let newVal = parseInt(e.target.value);
+              setLongRestTime(newVal);
+            }}
+          ></input>
+        </div>
+      </div>
     </div>
   );
 }
